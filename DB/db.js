621676a761878fb1e1900b5e9225456e3daea3c1
JoyDTechGirl <samsonjoyuwaoma7@gmail.com>
2025-02-05 14:57:07 +0100

    i made new change

--- DB/db.js
+++ DB/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("sql5760455", "sql5760455", "ShKAD5vZHK", {
+const sequelize = new Sequelize("LearningPlatform", "root", "Jobe7Joy7", {
   host: "localhost",
   dialect: "mysql",
 });
