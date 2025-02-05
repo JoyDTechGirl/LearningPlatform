6798bc07c55142670974bf43b338bfb59823fa33
JoyDTechGirl <samsonjoyuwaoma7@gmail.com>
2025-02-05 18:04:08 +0100

    i made a change

--- Router/usersRouter.js
+++ Router/usersRouter.js
@@ -1,9 +1,13 @@
-const {createUser,getAllUser,getOne} = require('../Controller/usersController')
+const {
+  createUser,
+  getAllUser,
+  getOne,
+} = require("../Controller/usersController");
 
-const router = require('express').Router()
+const router = require("express").Router();
 
-router.post('/users',createUser)
-router.get('/users',getAllUser)
-router.get('/users/:id',getOne)
+router.post("/users", createUser);
+router.get("/users", getAllUser);
+router.get("/users/:id", getOne);
 
 module.exports = router;
